03146ce2c71d985e76d21ef2d21ba1103b2ad7e7
Sojeong Cheon <sojeong.cheon94@gamil.com>
2020-01-28 16:48:00 +0900

    change text button to icon button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 import Main from './components/main'
+import { FaHome, FaRegLaughSquint, FaStickerMule, FaHorse, FaIdCard, FaRegFileCode, FaRegSun, FaYoutube, FaCommentDots, FaRegEnvelope } from "react-icons/fa"
 import './App.css';
 
 class App extends Component {
@@ -10,19 +11,19 @@ class App extends Component {
         <div className="App">
         <header className="ToolBar">
             <Link to = "/">
-              <button className="HomeButton">Home</button>
+              <button className="HomeButton"><FaHome /></button>
             </Link>
             <Link to = "/me">
-              <button className="MeButton">Me</button>
+              <button className="MeButton"><FaHorse /></button>
             </Link>
             <Link to = "/skills">
-              <button className="SkillsButton">Skills</button>
+              <button className="SkillsButton"><FaRegFileCode /></button>
             </Link>
             <Link to = "/projects">
-              <button className="ProjectsButton">Projects</button>
+              <button className="ProjectsButton"><FaYoutube /></button>
             </Link>
             <Link to = "/contact">
-              <button className="ContactButton">Contact</button>
+              <button className="ContactButton"><FaRegEnvelope /></button>
             </Link>
         </header>
         <div className="PageContents">
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -10,14 +10,73 @@ const particleOpt = {
                 enable: true,
                 value_area: 1000
             }
+        },
+        size: {
+            value: 5,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 2,
+              size_min: 0,
+              sync: false
+            }
+        },
+        color: {
+            value: "#ffffff"
+        },
+        shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#ffffff"
+            },
+        polygon: {
+            nb_sides: 5
+            },
+        opacity: {
+            value: 0.3,
+            random: false,
+            anim: {
+                enable: true,
+                speed: 0.2,
+                opacity_min: 0,
+                sync: false
+            }
+            },
+        line_linked: {
+            enable: false,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1
+            },
+        move: {
+        enable: true,
+        speed: 0.5,
+        direction: "none",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200
+                }
+            }
         }
     }
 }
+
 class Home extends Component{
     render() {
         return(
             <div className = "HomeContents">
-                <h1>Home Page</h1>
+                <div className = "HomeText">
+                    <h1>Hi,</h1>
+                    <h1>I'm <text className="Name">Sojeong Cheon <text className="Smile">;)</text></text></h1>
+                    <h1>Medical Image Processing Researcher.</h1>
+                </div>
                 <Particles 
                 style = {{
                     position: "absolute",
